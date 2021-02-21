89f57320f447ab1feb8e13aadb60b9fb4893dca0
jeast00 <38165949+jeast00@users.noreply.github.com>
2021-02-21 10:03:46 -0500

    passed tests

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let timeConvert = parseInt(string);
+  if (timeConvert < 12) {
+    return 'Good Morning';
+  } else if (timeConvert >= 12 && timeConvert <= 17) {
+    return 'Good Afternoon';
+  }
+    else if (timeConvert > 17 && timeConvert < 24) {
+      return 'Good Evening';
+    }
+}
+
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
